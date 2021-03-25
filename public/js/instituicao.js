d9dc8d85f06b63dbe6757024eabc9c53a0c7f095
João Amador <nnettoolliveira@gmail.com>
2021-03-25 04:08:39 -0300

    adicionandoo funcionalidades no map

--- public/js/instituicao.js
+++ public/js/instituicao.js
@@ -1,11 +1,28 @@
-const map = L.map("mapid").setView([51.505, -0.09], 13);
+// criando mapa
+const map = L.map("mapid").setView([-5.7957658, -35.2079482, 15], 15);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+// criando e adicionando tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-L.marker([51.5, -0.09])
+// criando icone
+const icon = L.icon({
+  iconUrl: "./public/img/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+// criando sobreposição de pop-up
+const popup = L.popup({
+  closeButton: false,
+  className: "map-popup",
+  minWidth: 240,
+  minHeight: 240,
+}).setContent(
+  'Casa Durval Paiva <a href="info-intituicao.html?id=1" class="choose-instituicao"> <img src="./public/img/next.svg" </a>'
+);
+
+// criando e adicionar marcador
+L.marker([-5.7957658, -35.2079482, 15], { icon: icon })
   .addTo(map)
-  .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
-  .openPopup();
+  .bindPopup(popup);
