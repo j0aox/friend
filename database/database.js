70a873c2da19f12133514493180235464ae848b1
João <nnettoolliveira@gmail.com>
2021-04-28 05:08:01 -0300

    removendo arquivos

--- database/database.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require('sequelize');
-
-const connection = new sequelize('amigo','root','root', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-
-module.exports = connection;
