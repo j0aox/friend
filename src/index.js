d48f6bb6477bdbe91964a1a6f610d895a64689d7
João <nnettoolliveira@gmail.com>
2021-04-30 03:42:48 -0300

    add funcionalidades ao index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const connection = require("./database/database");
-const path = require('path'); 
+const path = require('path');
+const router = require('./routes');
+
+// Iniciando o express
+const app = express();
+
+// Database
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com sucesso!");
+    }).catch((error) => {
+        console.log(error);
+    });
 
 // View engine
 app.set('views', path.join(__dirname, 'views'));
@@ -15,10 +28,14 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-    res.render("index");
+// Rotas
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
+app.use(router);
+
+// Servidor
 app.listen(5000, () => {
     console.log("O servidor está rodando!");
 });
