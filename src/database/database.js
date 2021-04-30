4771393813a664a9a98e1f0e9c30cbd31ef532a3
João <nnettoolliveira@gmail.com>
2021-04-30 03:46:00 -0300

    alterando db

--- src/database/database.js
+++ src/database/database.js
@@ -5,13 +5,4 @@ const connection = new sequelize('amigo','root','root', {
     dialect: 'mysql'
 });
 
-// Database
-connection
-    .authenticate()
-    .then(() => {
-    console.log("Conexão feita com sucesso!");
-    }).catch((error) => {
-        console.log(error);
-    });
-
 module.exports = connection;
