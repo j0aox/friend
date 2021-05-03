26047aed4ce91c3a8c7d8b548104f221d18fc498
João <nnettoolliveira@gmail.com>
2021-05-02 21:13:18 -0300

    add funcionalidade para mostrar as Instituições na view

--- src/controllers/InstituicaoController.js
+++ src/controllers/InstituicaoController.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const Instituicao = require('../model/Instituicao');
 
 router.get('/instituicao', (req, res) => {
-    return res.render('instituicao');
+
+    Instituicao.findAll({ attributes: ['id', 'name', 'lat', 'lng']  }).then(institutions => {
+        return res.render('instituicao', {institutions});
+    });
+
 });
 
 module.exports = router;
