87d45fb451ea9cc5358d8e45a211093bb201e9b7
João <nnettoolliveira@gmail.com>
2021-05-02 21:11:40 -0300

    add rotas

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,15 @@ const router = express.Router();
 
 const instituicaoController = require('./controllers/InstituicaoController');
 const createInstituicaoController  = require('./controllers/createInstituicaoController');
-const infoInstituicaoController  = require('./controllers/info-instituicao');
+const infoInstituicaoController  = require('./controllers/InfoInstituicaoController');
+
+//const model = require('./model/Instituicao');
 
 router.get('/instituicao', instituicaoController);
 router.get('/create-instituicao', createInstituicaoController);
 router.get('/info-instituicao', infoInstituicaoController);
+
+router.post('/create-instituicao/save', createInstituicaoController);
+
 
 module.exports = router;
