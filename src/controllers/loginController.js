0adbe5c768bd38c6f141677f94985cd2e5a561c8
João <nnettoolliveira@gmail.com>
2021-06-09 22:06:29 -0300

    Arquivos do formulário de login do usuário

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../model/User');
 
 router.get('/login', (req, res) => {
     return res.render('login');
+});
+
+router.get('/create-login', (req, res) => {
+    return res.render('create-login');
 });
 
 module.exports = router;
