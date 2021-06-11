2391e967ee7c4a04ae38bff7b74b529b165f3283
João <nnettoolliveira@gmail.com>
2021-06-10 22:34:48 -0300

    Adicionando novas funcionalidades do login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 
 router.get('/login', (req, res) => {
     return res.render('login');
@@ -8,6 +9,28 @@ router.get('/login', (req, res) => {
 
 router.get('/create-login', (req, res) => {
     return res.render('create-login');
+});
+
+router.post('/create-login/save', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    User.create({
+        email: email,
+        password: hash,
+    }).then(() => {
+        res.redirect('/create-instituicao');
+    }).catch((err) => {
+        //console.log(err);
+        res.redirect('/');
+    });
+});
+
+router.get('/', (req, res) => {
+    return res.render('index');
 });
 
 module.exports = router;
