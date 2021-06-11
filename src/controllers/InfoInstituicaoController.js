cbbda65bc232b98cd432db1cbc14f9f2e4bcc895
João <nnettoolliveira@gmail.com>
2021-06-10 22:37:03 -0300

    Excluindo linhas desnecessária

--- src/controllers/InfoInstituicaoController.js
+++ src/controllers/InfoInstituicaoController.js
@@ -4,15 +4,7 @@ const Instituicao = require("../model/Instituicao");
 
 router.get("/info-instituicao", async (req, res) => {
   const id = req.query.id;
-  /*
-    Instituicao.findAll({ where: {
-        id: 2
-      } 
-      ['id', 'name', 'lat', 'lng']  }).then(institutions => {
-        return res.render('info-instituicao', {institutions});
-    });
-*/
-  //return res.render('info-instituicao');
+
   const institution = await Instituicao.findByPk(id);
   
   return res.render("info-instituicao", { institution });
