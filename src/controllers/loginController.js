09de97ecc837e906ab7cd29b6e7210526f76aeda
João <nnettoolliveira@gmail.com>
2021-06-11 21:21:11 -0300

    Modificação no front e back

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,36 +1,66 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../model/User');
-const bcrypt = require('bcryptjs');
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
 
-router.get('/login', (req, res) => {
-    return res.render('login');
+router.get("/login", (req, res) => {
+  return res.render("login");
 });
 
-router.get('/create-login', (req, res) => {
-    return res.render('create-login');
+router.get("/create-login", (req, res) => {
+  return res.render("create-login");
 });
 
-router.post('/create-login/save', (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+router.post("/create-login/save", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
+  User.findOne({ where: { email: email } }).then((user) => {
+    if (user == undefined) {
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(password, salt);
 
-    User.create({
+      User.create({
         email: email,
         password: hash,
-    }).then(() => {
-        res.redirect('/create-instituicao');
-    }).catch((err) => {
-        //console.log(err);
-        res.redirect('/');
-    });
+      }).then(() => {
+          res.redirect("/create-instituicao");
+         })
+        .catch((err) => {
+          res.redirect("/");
+        });
+
+    } else {
+        res.redirect("/create-login/save")
+    }
+  });
 });
 
-router.get('/', (req, res) => {
-    return res.render('index');
+router.get("/", (req, res) => {
+  return res.render("index");
 });
+
+router.post("/autenticate", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({where:{email: email}}).then( user => {
+    if (user != undefined) {
+      const correct = bcrypt.compareSync(password, user.password);
+
+      if (correct) {
+        req.session.user = {
+          id: user.id,
+          email: user.email
+        }
+        res.json(req.session.user);
+      } else {
+        res.redirect("/login");
+      }
+    } else {
+      res.redirect("/login");
+    }
+  });
+})
 
 module.exports = router;
