ee3f7b0fd8f10e4adac1c47edfbb76187a08183e
João <nnettoolliveira@gmail.com>
2021-06-18 21:17:38 -0300

    Aletrando tamanho da sessão

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(session({
     secret: "juntosParaOBem", 
     resave: true,
     saveUninitialized: true,
-    cookie: { maxAge: 30000000 }
+    cookie: { maxAge: 300000 }
 }));
 
 // Database
